1665a1ccc3bc1e5261e94412dbfbed7d6d6f4731
escapedcat <github@htmlcss.de>
2018-06-11 18:11:29 +0200

    feat(explorer): add blocks api and basic vue project

---
+++ lib/modules/mods/explorer-refactor/src/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page">
+    <router-view />
+  </div>
+</template>
+
+<script>
+// import Vue from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+  },
+}
+
+</script>
