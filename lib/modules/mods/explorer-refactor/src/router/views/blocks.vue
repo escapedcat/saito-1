116f152c9e0caa9feb800ed4a9586194bd388792
escapedcat <github@htmlcss.de>
2018-06-14 17:14:31 +0200

    feat(blocks): add basic html and fetch blocks-json

---
+++ lib/modules/mods/explorer-refactor/src/router/views/blocks.vue
@@ -0,0 +1,58 @@
+<script>
+// import Layout from '@layouts/main'
+
+export default {
+  // components: { Layout },
+  data() {
+    return {
+      blocks: [],
+    }
+  },
+  created() {
+    this.getBlocks()
+  },
+  methods: {
+    getBlocks() {
+      const vm = this
+
+      fetch('http://localhost:12101/explorer/json/blocks').then(response => response.json().then((json) => {
+        vm.blocks = json
+      }))
+    },
+  },
+}
+</script>
+
+<template>
+  <!-- <Layout> -->
+  <div>
+
+    <h1>Blocks</h1>
+
+    <ul>
+      <li
+        v-for="item in blocks"
+        :key="item.id"
+        :class="{'is-longest': item.isLongest}"
+        tabindex="0"
+      >
+        <ul>
+          <li>
+            id: {{ item.id }}
+          </li>
+          <li>
+            hash: {{ item.hash }}
+          </li>
+          <li v-if="item.prevHash">
+            previous hash: {{ item.prevHash }}
+          </li>
+          <li v-else>
+            ¯\_(ツ)_/¯
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+  </div>
+  <!-- </Layout> -->
+</template>
